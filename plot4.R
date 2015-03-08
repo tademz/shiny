7a6daa3b1ff274d2e0c63cf6fb392ec0618db706
jorgml <jorgml@users.noreply.github.com>
2015-03-08 18:59:05 -0400

    Create plot4.R
    Plot 4

---
+++ plot4.R
@@ -0,0 +1,35 @@
+# Read in all the data
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";",
+                   stringsAsFactors=FALSE, na.strings=c("?"))
+
+# Below we only need data from two days
+sdata <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
+
+# Create a new column with the combined date and time
+sdata$datetime <- mapply(function(s1, s2){as.POSIXct(paste(s1, s2), format = "%d/%m/%Y %H:%M:%S")},
+                         sdata$Date,sdata$Time)
+
+png("plot4.png")
+par(mfrow = c(2,2))
+
+plot(sdata$datetime, sdata$Global_active_power, type="l", xaxt = "n",
+     ylab = "Global Active Power", xlab = "")
+axis.POSIXct(1, as.POSIXct(sdata$datetime, origin="1970-01-01"))
+
+plot(sdata$datetime, sdata$Voltage, type="l", xaxt = "n",
+     ylab = "Voltage", xlab = "datetime")
+axis.POSIXct(1, as.POSIXct(sdata$datetime, origin="1970-01-01"))
+
+plot(sdata$datetime, sdata$Sub_metering_1, type="l", xaxt = "n",
+     ylab = "Energy sub metering", xlab = "")
+lines(sdata$datetime, sdata$Sub_metering_2, col = "red")
+lines(sdata$datetime, sdata$Sub_metering_3, col = "blue")
+axis.POSIXct(1, as.POSIXct(sdata$datetime, origin="1970-01-01"))
+legend("topright", lty = 1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       bty = "n")
+
+plot(sdata$datetime, sdata$Global_reactive_power, type="l", xaxt = "n",
+     ylab = "Global_reactive_power", xlab = "datetime")
+axis.POSIXct(1, as.POSIXct(sdata$datetime, origin="1970-01-01"))
+dev.off()
